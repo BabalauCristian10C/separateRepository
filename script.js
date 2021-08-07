43cb1630308752b68d160c1a31d25ff8a6c58c1d
Chris Babalau <chrisbabalau@Chriss-MacBook-Air.local>
2021-08-07 19:44:53 +0300

    Добавлена рекурския

--- script.js
+++ script.js
@@ -1,38 +1,32 @@
 'use strict';
 
-alert("Угадай число от 1 до 100");
-
-
 function randomNumber(){
     return Math.ceil(Math.random() * (100 - 1) + 1);
 }
 
-let game = function (randomNumber) {
-    let randomNum = randomNumber;
+let game = function () {
+    alert("The game started!");
+    let randomNum = randomNumber();
     console.log(randomNum);
-    
-    let userAnswer = (function () {
-        return prompt("Гадай", "1");
-    }());
 
-    
-
-    if (+userAnswer === randomNum) {
-        alert('Поздравляю, Вы угадали!!!');
-        return true; 
-    } else if (+userAnswer > randomNum) {
-        alert("The number is smaller");
-        return game(randomNum);
-    } else if (+userAnswer < randomNum && userAnswer !== null) {
-        alert("The number is bigger");
-        return game(randomNum);
-    } else if(isNaN(+userAnswer)) {
-        alert("Введи число!");
-        return game(randomNum);
-    } else if(userAnswer === null){
-        alert("Game over");
-        return false;
-    }
+    let mainCicle = (function () {
+        let userAnswer = prompt("Гадай", "1");
+        if (+userAnswer === randomNum) {
+            alert('Поздравляю, Вы угадали!!!');
+            return;
+        } else if (+userAnswer > randomNum) {
+            alert("The number is smaller");
+        } else if (+userAnswer < randomNum && userAnswer !== null) {
+            alert("The number is bigger");
+        } else if(isNaN(+userAnswer)) {
+            alert("Введи число!");
+        } else if(userAnswer === null){
+            alert("Game over");
+            return;
+        }
+        mainCicle();
+    });
+    mainCicle();
 };
 
-game(randomNumber());
+game();
